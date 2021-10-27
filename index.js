affa3c0fb588f7d2e49f85012c19b3ff0d3398a6
silviaplabon <silviaplabon@gmail.com>
2021-10-27 19:16:51 +0600

    adding users at uniquesID

--- index.js
+++ index.js
@@ -6,40 +6,61 @@ const port = 4000;
 const cors = require('cors');
 app.use(cors());
 
-const { addUser, removeUser, getUser, getUsersInRoom, getFromAllUsers } = require('./users');
+const { addUser, removeUser, getUser, getUsersInRoom, getFromAllUsers, deleteAllPreviousData, senderIsAvailableReceiverRoom, getAllUser } = require('./users');
 
 io.on('connect', (socket) => {
-  console.log('connect')
+
+  //when a user is exist then it will create a room for this user.Then by roomInformation event user can get his room id. 
   socket.on('join', (name, callback) => {
     const room = Date.now() + Math.floor(Math.random() * 1000000)
     console.log(room)
-
+    //if any user exist
+    deleteAllPreviousData(name);
     const { error, user } = addUser({ id: socket.id, name, room });
     if (error) return callback(error);
     console.log(user.room, "user room information")
     socket.join(user.room);
-    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.name}.`, delivered: true, id: 1122 });
-    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!`, delivered: true, id: 1111 });
     socket.emit('roomInformation', { room: user.room, name: user.name });
-    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
     callback();
   });
 
 
-  socket.on('sendMessageReceiver', (data) => {
-    socket.broadcast.emit('messageReceiver', data)
-  });
 
-  socket.on('sendRequest', (data,callback) => {
+  socket.on('sendRequest', (data, callback) => {
     console.log(data, "data ")
-    const usersdata = getFromAllUsers(data.receiver);
-    console.log(usersdata, "users data", data.room, data.sender, data.receiver, "users data and dash dash")
-    const { error, user } = addUser({ id: usersdata.id, name: usersdata.name, room: data.room });
-    if (error) return callback(error);
-    console.log(user.room, "user room information")
-    socket.join(user.room);
-    // socket.broadcast.to(user.room).emit('sendRequestReceiver', {  sender: data.sender, room: data.room  });
-    callback();
+    //from all users getting data of receiver with different room
+    const usersdata = getFromAllUsers(data.receiver, data.room);
+    console.log(usersdata, "Receiver data is available here")
+
+    if (usersdata) {
+
+      const roomCreated = senderIsAvailableReceiverRoom(usersdata.room, data.room, data.sender, data.receiver)
+      console.log(roomCreated, "room is created")
+      if (roomCreated == false) {
+        console.log(usersdata, "users data", data.room, data.sender, data.receiver, "users data and dash dash")
+        const { error, user } = addUser({ id: usersdata.id, name: usersdata.name, room: data.room });
+        if (error) return callback(error);
+        console.log(user.room, "user room information")
+        socket.join(user.room);
+        // socket.broadcast.to(user.room).emit('sendRequestReceiver', {  sender: data.sender, room: data.room  });
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.name}.`, delivered: true, id: 1122 });
+        io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!`, delivered: true, id: 1111 });
+        socket.emit('roomInformation', { room: user.room, name: user.name });
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+      }
+      callback();
+      
+
+
+     console.log(getAllUser());
+
+
+    }
+    else {
+      const error = 'user is not available for chat';
+      return callback(error)
+    }
+
   })
 
   socket.on('online', (data) => {
@@ -54,9 +75,8 @@ io.on('connect', (socket) => {
 
   socket.on('sendMessage', (data, callback) => {
     // console.log(message,name,"message name")
-    console.log(data, "data from send message")
-    socket.broadcast.to(data?.room).emit('message', { id: data?.id, user: data?.sender, text: data?.message, sendTime: data?.sendTime, delivered: false });
-    
+     getAllUser()
+    socket.broadcast.emit('message', { id: data?.id, user: data?.sender, text: data?.message, sendTime: data?.sendTime, delivered: false });
     callback();
   });
 
@@ -78,9 +98,10 @@ io.on('connect', (socket) => {
   });
 
   socket.on('disconnect', () => {
+    console.log(socket.id, "socket id")
     const user = removeUser(socket.id);
-
     console.log(socket.id, "socket id from remove user")
+
     if (user) {
       io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
       io.to(user.room).emit('offline', { state: 'offline', name: user.name });
--- users.js
+++ users.js
@@ -2,32 +2,18 @@ const users = [];
 console.log(users)
 const addUser = ({ id, name, room }) => {
     console.log(name, id, room)
-    const roomda=room;
     // name = name.trim().toLowerCase();
     // room = room.trim().toLowerCase();
-   
     const existingUser = users.find((user) => user.room === room && user.name === name);
     const usersInARoom = users.filter((user) => user.room === room);
-    // const roomFinding = users.find((user) => user.room === room);
-    //comment out 
-    // const roomdata = room.split('_');
-    // const newroom = roomdata[1] + '_' + roomdata[0];
 
-    let roomInformation;
-    // console.log(newroom,"newroom")
     if (!name || !room) return { error: 'Username and room are required.' };
     if (existingUser) return { error: 'Username is taken.' };
     if (usersInARoom.length >=2 ) return { error: 'Only two person are allowed' }
-    // const existingRoomSender = users.find((user) => user.room === room);
-    // const existingRoomReceiver = users.find((user) => user.room === newroom);
-    // if (existingRoomSender) {
-    //     room=roomInformation
-    // }
-    // if(existingRoomReceiver){
-    //      room=newroom;
-    // }
+  
     const user = { id, name, room };
     console.log(user,"user information available")
+
     if(usersInARoom.length<2 ){
         users.push(user);
         console.log(users,"users information")
@@ -36,24 +22,45 @@ const addUser = ({ id, name, room }) => {
 }
 
 const removeUser = (id) => {
-    console.log(users,"users from remove user")
     const index = users.findIndex((user) => user.id === id);
     console.log(index);
     if (index !== -1) return users.splice(index, 1)[0];
     console.log(users,"after removing")
+
 }
 
 const getUser = (id) =>{
     const datas=users.find((user) => user.id=== id);
     return datas;
 };
-
-const roomIsAlreadyCreated=()=>{
-    const datas=users.find((user) => user.id=== id && user.name==name);
-
+const senderIsAvailableReceiverRoom=(receiverroom,senderroom,sender,receiver)=>{
+    const senderIsAvailableReceiver=users.find((user) => user.name===sender && user.room===receiverroom);
+    const receiverIsAvailableSender=users.find((user) => user.name===receiver && user.room===senderroom);
+   if(senderIsAvailableReceiver || receiverIsAvailableSender){
+       return true;
+   }
+   else{
+       return false;
+   }
 }
 
 const getUsersInRoom = (room) => users.filter((user) => user.room === room);
-const getFromAllUsers=(name)=>users.find((user)=>user.name===name)
 
-module.exports = { addUser, removeUser, getUser, getUsersInRoom,getFromAllUsers,roomIsAlreadyCreated};
+const getFromAllUsers=(name,room)=>users.find((user)=>user.name===name&& user.room!==room);
+
+const getAllUser=()=>{ console.log(users,"users from get all users")}
+
+console.log(users,"users in a room")
+const deleteAllPreviousData=(name)=>{
+  for(var i=0;i<users.length;i++){
+      if(users[i].name==name){
+          console.log(users[i])
+          console.log(users,"users delete all previous data")
+          users.splice(i,1)
+      }
+  }
+}
+
+
+
+module.exports = { addUser,getAllUser, removeUser, getUser, getUsersInRoom,getFromAllUsers,senderIsAvailableReceiverRoom,deleteAllPreviousData};
